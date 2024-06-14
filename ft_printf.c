23a568adb363aa3891acf5267e56b5c34ab3d4eb
Timur Haziiev <thaziiev@c2r6s4.42london.com>
2024-06-14 19:22:07 +0100

    gitignore ssh.pub

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,49 @@
 /*   By: thaziiev <thaziiev@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 20:40:31 by thaziiev          #+#    #+#             */
-/*   Updated: 2024/06/03 20:57:04 by thaziiev         ###   ########.fr       */
+/*   Updated: 2024/06/13 18:32:05 by thaziiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdarg.h>
+
+print_char(char c)
+{
+	
+}
+
+print_format(char specifier, va_list ap)
+{
+	int count;
+	
+	count = 0;
+	if (specifier == 'c')
+		count += print_char(va_arg(ap, int));
+}
 
 int ft_printf(const char *format, ...)
 {
-	return ((int)*format);
+	va_list	ap;
+	int 	count;
+	
+	va_start(ap, format);
+	count = 0;
+	while(*format)
+	{
+		if (*format == '%')
+			count += print_format(*(++format), ap);
+		else
+			count += write(1, format, 1);
+		format++;
+	}
+	return (count);
 }
 
 int main()
 {
+	char ch;
+
+	ch = 'a';
+	ft_printf("Printf me %c", ch);
 	return (1);
 }
